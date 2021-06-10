52a28e567950e718f63df7a1c73b230270821c68
DanJaegerDev <59783252+DanJaeger@users.noreply.github.com>
2021-06-10 12:02:20 -0500

    Splash Actualizado

--- app/src/main/java/com/example/dskaraoke/SplashActivity.java
+++ app/src/main/java/com/example/dskaraoke/SplashActivity.java
@@ -2,15 +2,29 @@ package com.example.dskaraoke;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class SplashActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
+        setContentView(R.layout.activity_splash);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 5000);
     }
 }
